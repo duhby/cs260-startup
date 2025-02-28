876aebb9b6a0247db352bd8d147adbfeaa5e6ddb
Josh <epikmemez@outlook.com>
2025-02-27 22:11:54 -0700

    feat: leaderboard page

--- src/leaderboard/leaderboard.jsx
+++ src/leaderboard/leaderboard.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export function Leaderboard() {
+  const [scores, setScores] = useState([]);
+  const defaultText = "No scores yet. Be the first!";
+  const loggedInUser = localStorage.getItem("username");
+
+  useEffect(() => {
+    const res = localStorage.getItem("scores");
+    if (res) {
+      setScores(JSON.parse(res));
+    }
+  }, []);
+
+  const scoreRows = [];
+  if (scores.length) {
+    for (const [i, score] of scores.entries()) {
+      scoreRows.push(
+        <tr
+          key={i}
+          className={
+            score.username === loggedInUser
+              ? "bg-gray-800 font-bold"
+              : "bg-gray-700"
+          }
+        >
+          <td>{i + 1}</td>
+          <td>{score.username}</td>
+          <td>{score.score}</td>
+          <td>{score.date}</td>
+        </tr>
+      );
+    }
+  } else {
+    scoreRows.push(
+      <tr key="0">
+        <td colSpan="4">{defaultText}</td>
+      </tr>
+    );
+  }
+
   return (
     <main className="flex flex-col items-center text-center text-xl">
       <p className="mb-4">Login to see your scores on the leaderboard.</p>
-
       <table className="table-auto w-2xl bg-gray-800 rounded-sm">
         <thead>
           <tr className="text-purple-400 bg-gray-900">
@@ -14,26 +51,7 @@ export function Leaderboard() {
             <th>Date</th>
           </tr>
         </thead>
-        <tbody>
-          <tr className="bg-gray-800 font-bold">
-            <td>1</td>
-            <td>Himothy</td>
-            <td>100</td>
-            <td>10 minutes ago</td>
-          </tr>
-          <tr className="bg-gray-700">
-            <td>2</td>
-            <td>Jimbothy</td>
-            <td>68</td>
-            <td>3 days ago</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td>3</td>
-            <td>Malarkey</td>
-            <td>12</td>
-            <td>1 week ago</td>
-          </tr>
-        </tbody>
+        <tbody>{scoreRows}</tbody>
       </table>
     </main>
   );
