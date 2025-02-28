6d5dc9b1b8673206289223b93d73e5b329f7e574
Josh <epikmemez@outlook.com>
2025-02-27 22:56:11 -0700

    fix: /login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-export function Login() {
-  const [username, setUsername] = React.useState("");
+export function Login({ username, setUsername }) {
   const [password, setPassword] = React.useState("");
+  const usernameRef = React.useRef(username);
   const navigate = useNavigate();
 
   React.useEffect(() => {
-    setUsername(localStorage.getItem("username"));
+    if (username) {
+      navigate("/play");
+    }
   }, []);
-  if (username) {
+
+  const login = (e) => {
+    e.preventDefault();
+    localStorage.setItem("username", usernameRef.current.value);
+    setUsername(usernameRef.current.value);
     navigate("/play");
-  }
+  };
 
   return (
     <main className="flex flex-col items-center text-center text-xl">
@@ -20,30 +26,27 @@ export function Login() {
           Log in or create an account to share your scores and appear on the
           leaderboard.
         </p>
-        <form
-          className="flex flex-col items-center"
-          action="/play.html"
-          method="get"
-        >
+        <form className="flex flex-col items-center" onSubmit={login}>
           <input
             className="border-2 w-sm rounded-sm p-1 mb-2 focus:border-purple-400 focus:outline-none"
             type="text"
-            name="username"
-            id="username"
             placeholder="Username"
+            required
+            ref={usernameRef}
           />
           <input
             className="border-2 w-sm rounded-sm p-1 mb-2 focus:border-purple-400 focus:outline-none"
             type="password"
-            name="password"
-            id="password"
             placeholder="Password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <input
+          <button
             className="bg-purple-800 w-sm rounded-sm p-2 hover:cursor-pointer hover:rounded-md hover:bg-gray-800 hover:text-purple-400"
             type="submit"
-            value="Log in"
-          />
+          >
+            Log in
+          </button>
         </form>
       </div>
     </main>
